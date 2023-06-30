6cb522429a3ae5cd6484d670dc0721932da25dd9
mirshekarm <asreweb81@gmail.com>
2023-06-30 18:19:49 +0330

    [BUGFIX] Make some changes!

---
+++ src/WpfBlazorHybridTemplate/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BlazorTemplate.Shared.ViewModel.Pages.Counter;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace WpfBlazorHybridTemplate;
+
+public partial class App : Application
+{
+	private ServiceProvider _serviceProvider;
+
+	public App()
+	{
+		var services = new ServiceCollection();
+
+		ConfigureServices(services);
+
+		_serviceProvider = services.BuildServiceProvider();
+
+		Resources.Add("services", _serviceProvider);
+	}
+
+	private void ConfigureServices(ServiceCollection services)
+	{
+		services.AddWpfBlazorWebView();
+		services.AddSingleton<MainWindow>();
+		services.AddSingleton<CounterViewModel>();
+		services.AddSingleton<ClickViewModel>();
+	}
+
+	private void OnStartup(object sender, StartupEventArgs e)
+	{
+		var mainWindow =
+			_serviceProvider.GetService<MainWindow>();
+
+		if (mainWindow == null)
+			return;
+
+		mainWindow.Show();
+	}
+}
---
+++ src/WpfBlazorHybridTemplate/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BlazorTemplate.Shared.ViewModel.Pages.Counter;
+using System.Windows;
+
+namespace WpfBlazorHybridTemplate;
+
+public partial class MainWindow : Window
+{
+	public MainWindow(ClickViewModel clickState)
+	{
+		InitializeComponent();
+
+		ClickState = clickState;
+
+		clickState.AddStateChangeListener(() => ClickStateTextBlock.Text = clickState.ClickName);
+	}
+
+	public ClickViewModel ClickState { get; }
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		ClickState.SetClickName("Wpf clicked!");
+	}
+}
